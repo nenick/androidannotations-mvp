865a7a898693d6862b044f85031bb8d69c3bab16
Nico Küchler <kuchler.nico@gmail.com>
2017-03-19 01:11:42 +0100

    fix sample instrumentation test

--- mvp-plugin-sample/src/androidTest/java/de/nenick/androidannotations/plugin/mvp/ExampleInstrumentedTest.java
+++ mvp-plugin-sample/src/androidTest/java/de/nenick/androidannotations/plugin/mvp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("de.nenick.androidannotations.plugin.mvp", appContext.getPackageName());
+        assertEquals("de.nenick.androidannotations.plugin.mvp.sample", appContext.getPackageName());
     }
 }
