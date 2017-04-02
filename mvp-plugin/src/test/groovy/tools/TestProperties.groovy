fa26887edc76a0e83dd41eb784bc4ad0a0593b30
Nico Küchler <kuchler.nico@gmail.com>
2017-04-02 22:35:58 +0200

    provide single source for plugin version

--- mvp-plugin/src/test/groovy/tools/TestProperties.groovy
+++ mvp-plugin/src/test/groovy/tools/TestProperties.groovy
@@ -2,12 +2,19 @@ package tools
 
 trait TestProperties implements TestProperties1 {
 
+
     // WOW? no more than 9 properties or it makes boom
     def androidProjectName = "test-app"
     String androidApplicationProjectId = "de.nenick.test.application"
     def androidLibraryProjectId = "de.nenick.test.library"
-    def pluginVersion = "1.0.0-SNAPSHOT"
-    def androidPluginVersion = "2.2.3"
+    def pluginVersion = readProjectVersion() + "-SNAPSHOT"
+    def androidPluginVersion = "2.3.0"
     def compileSdkVersion = "25"
     def buildToolsVersion = "25.0.1"
+
+    def readProjectVersion() {
+        Properties properties = new Properties()
+        properties.load(getClass().getClassLoader().getResourceAsStream("mvp-plugin.properties"))
+        return properties.version
+    }
 }
